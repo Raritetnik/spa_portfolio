0dc6fb4fd96d946cf29a110e509f9c3b9aecba95
Mykhaylo Kuzmin <xxxxxxxxx@gmail.com>
2023-03-22 18:48:41 -0400

    Correction d'aglaicisme

--- src/frontend/AboutPage.js
+++ src/frontend/AboutPage.js
@@ -9,7 +9,7 @@ const AboutPage = () => {
                     className='rounded-lg mx-auto' />
                 </div>
                 <div className="about md:flex-col md:justify-center text-center max-w-none lg:max-w-md lg:text-left">
-                    <h2 className="text-green-600 text-xl font-bold mb-3">ABOUT ME</h2>
+                    <h2 className="text-green-600 text-xl font-bold mb-3">À propos de moi</h2>
                     <h3 className="mb-3 font-bold text-xl">Concepteur et Programmeur de Sites WEB</h3>
                     <p className="font-mono">En tant que développeur Front-End et Back-End junior, je possède un arsenal impressionnant de compétences en HTML, CSS, JavaScript, React, Next.JS, Node.JS, Bootstrap, Laravel, PHP, Java, RESTful API, Tailwind CSS et SCSS. J'excelle dans la conception et la maintenance de sites Web réactifs offrant une expérience utilisateur fluide. Mon expertise réside dans la création d'interfaces dynamiques et engageantes en écrivant un code propre et optimisé et en utilisant des outils et techniques de développement de pointe. Je fait, également, partie du groupe qui prospère dans la collaboration avec des équipes pluridisciplinaires pour produire des applications Web exceptionnelles.</p>
                 </div>
