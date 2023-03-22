8b94573e8eaebf945f846c01df394a3234307d3e
Mykhaylo Kuzmin <xxxxxxxxx@gmail.com>
2023-03-22 19:34:12 -0400

    Importation de mon image profil

--- src/frontend/HomePage.js
+++ src/frontend/HomePage.js
@@ -2,9 +2,13 @@ import { BsLinkedin, BsGithub } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
+    const borderWave = {
+        animation: 'wave-block',
+    }
+
     return(
         <section className="block">
-            <div className="container mx-auto px-20 flex flex-col items-center mt-10 lg:flex-row lg:justify-between">
+            <div className="container mx-auto px-20 flex flex-col items-center mt-16 lg:flex-row lg:justify-between">
                 <div style={{ maxWidth: 400}} className='d-block'>
                     <h1 className="text-3xl font-black pb-4">Front-End et Back-End Développeur</h1>
                     <p className="text-gray-500 pb-4 font-mono">Bonjour, je m'appelle Mykhaylo Kuzmin et je suis concepteur et programmeur de sites web.
@@ -19,7 +23,10 @@ const HomePage = () => {
                     </small>
                 </div>
                 <div className="">
-                    <img src="https://cdn-icons-png.flaticon.com/512/1995/1995515.png" style={{ width: 400, height: 400}}/>
+                    {/*<img src="https://cdn-icons-png.flaticon.com/512/1995/1995515.png" style={{ width: 400, height: 400}}/>*/}
+                    <img src="./assets/profile_img_2.jpg"
+                    className='animate-pulse'
+                    style={{ width: 400, height: 400, animation: 'wave-block 5s linear infinite', border: '2px solid var(--color__title)' }}/>
                 </div>
             </div>
         </section>
