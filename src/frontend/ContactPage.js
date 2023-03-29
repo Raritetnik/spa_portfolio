0f2b10166b7a7d21269ded38aab3cb881eafb597
Mykhaylo Kuzmin <xxxxxxxxx@gmail.com>
2023-03-29 12:50:21 -0400

    turn off message

--- src/frontend/ContactPage.js
+++ src/frontend/ContactPage.js
@@ -31,14 +31,14 @@ const ContactPage = () => {
                     <form className='w-full lg:w-2/3 flex flex-col mb-8' onSubmit={saveMessage}>
                         <div className='w-full flex mb-4 gap-x-4'>
                             <input type="text" placeholder='Nom complet' className='w-full border-2 border-green-600 py-2 px-2 rounded-md'
-                                onChange={e => setNom(e.target.value)}/>
+                                onChange={e => setNom(e.target.value)} disabled/>
                             <input type="text" placeholder='Courriel ou Téléphone' className='w-full  border-2 border-green-600 py-2 px-2 rounded-md'
-                                onChange={e => setContact(e.target.value)}/>
+                                onChange={e => setContact(e.target.value)} disabled/>
                         </div>
                         <input type="text" className='py-2 px-2 mb-4 border-2 border-green-600 rounded-md' placeholder='Sujet de contacte'
-                            onChange={e => setTitre(e.target.value)}/>
+                            onChange={e => setTitre(e.target.value)} disabled/>
                         <textarea cols="30" rows="10" className='p-2 mb-4 border-2 border-green-600 rounded-md' placeholder='Votre message'
-                            onChange={e => setMessage(e.target.value)}></textarea>
+                            onChange={e => setMessage(e.target.value)} disabled></textarea>
                         <input type="submit" value='Envoyer le message'
                             className='px-4 py-2 mx-auto w-200 border-2 border-green-600 rounded-full hover:bg-green-600 hover:text-white'/>
                     </form>
